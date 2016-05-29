a00e352e0e516d1efd985b8ac8d03009f1d88131
Jayesh Kawli <j.kawli@gmail.com>
2016-05-29 18:25:03 -0400

    Initial commit

---
+++ JKiOSFramework/JKiOSFramework.h
@@ -0,0 +1,19 @@
+//
+//  JKiOSFramework.h
+//  JKiOSFramework
+//
+//  Created by Jayesh Kawli Backup on 5/29/16.
+//  Copyright © 2016 Jayesh Kawli Backup. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JKiOSFramework.
+FOUNDATION_EXPORT double JKiOSFrameworkVersionNumber;
+
+//! Project version string for JKiOSFramework.
+FOUNDATION_EXPORT const unsigned char JKiOSFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JKiOSFramework/PublicHeader.h>
+
+
